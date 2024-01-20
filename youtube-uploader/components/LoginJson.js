7247ea242185362cabc2a245b8dc2e4f6004db34
utylee <utylee@gmail.com>
2024-01-20 19:10:06 +0900

    chg: add LoginJson.js and add json websocket refreshing function

---
+++ youtube-uploader/components/LoginJson.js
@@ -0,0 +1,116 @@
+import { Flex, HStack } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+
+const LoginJson = ({ json_date, auth_status, setSocketConnected, ws }) => {
+  console.log("LoginJson::auth_status::" + auth_status);
+  const [isHover, setIshover] = useState(false);
+
+  const msging = () => {
+    let result = "login.json   " + json_date;
+    if (auth_status === "processing") {
+      // result = "authorizing...";
+      return <Flex>authorizing...</Flex>;
+    } else {
+      let clr = isHover ? "gray.400" : "purple.700";
+      // let clr_hover = "gray.100";
+      return (
+        <HStack>
+          {/* <Flex color="purple.700" alignSelf="end" sx={{ fontSize: "0.7em" }}> */}
+          <Flex mr="0em">login.json</Flex>
+          <Flex color={clr} alignSelf="end" sx={{ fontSize: "0.8em" }}>
+            {json_date}
+          </Flex>
+        </HStack>
+      );
+    }
+  };
+
+  const bging = () => {
+    let result = "0";
+    if (auth_status === "processing" && isHover !== true) {
+      result = "pink.700";
+    } else if (isHover === true) {
+      result = "purple.800";
+    }
+    return result;
+  };
+
+  const coloring = () => {
+    let result = "gray.600";
+    if (auth_status === "processing" && isHover !== true) {
+      result = "gray.100";
+    } else if (isHover === true) {
+      result = "gray.500";
+    }
+    return result;
+  };
+
+  const sendRefreshing = async () => {
+    console.log("LoginJson.js::sendRefreshing");
+
+    //클라이트 리프레싱 루틴도 추가합니다
+    console.log("LoginJson.js::sendRefreshing::ws socket is ", ws.current);
+    console.log(ws.current);
+    // if (is_connected == false) {
+    if (ws.current.readyState == 3) {
+      console.log(
+        "LoginJson::sendRefreshing::ws socket not connected so refreshing and reconnect"
+      );
+
+      // ws를 null로 할당하고 setSocketConnected 함수를 호출해서 소켓생성 useEffect를
+      // 실행하게끔합니다
+      ws.current = null;
+      setSocketConnected(false);
+      // is_connected.current = false;
+      // refresh_function(Math.random());
+    }
+
+    const res = await fetch("/youtube/uploader/loginjson");
+    // const res = await fetch("/uploader/api/listjs");
+    // const js = await res.json();
+    const txt = await res.text();
+    console.log("LoginJson::sendRefreshing::after loginjson fetch::...");
+    console.log(txt);
+
+    // setFiles([...js]);
+    // console.log("files:");
+    // console.log(files);
+  };
+  return (
+    <>
+      {/* bg: isHover ? "purple.800" : 0, */}
+      {/* color: isHover ? "gray.500" : "gray.600", */}
+
+      {/*
+        sx={{
+          bg: () => bging(),
+          cursor: isHover ? "pointer" : "none",
+          color: () => coloring(),
+        }}
+		*/}
+      <Flex
+        border="1px"
+        rounded={["0.5em", "0.5em", "0.7em"]}
+        w={["16em", "16em", "30em"]}
+        h={["1.5em", "1.5em", "2em"]}
+        px="1em"
+        justify="center"
+        alignItems="center"
+        borderColor="purple.800"
+        overflow="hidden"
+        onMouseEnter={() => setIshover(true)}
+        onMouseLeave={() => setIshover(false)}
+        onClick={() => sendRefreshing()}
+        sx={{
+          bg: bging(),
+          cursor: isHover ? "pointer" : "none",
+          color: coloring(),
+        }}
+      >
+        {msging()}
+      </Flex>
+    </>
+  );
+};
+
+export default LoginJson;
--- youtube-uploader/pages/index.js
+++ youtube-uploader/pages/index.js
@@ -7,8 +7,8 @@ import LoginJson from "../components/LoginJson";
 export default function Home() {
   const [files, setFiles] = useState({});
   // const [files, setFiles] = useState({ json_date: "", files: [] });
-  const [myconfirm, setMyconfirm] = useState("");
-  const [socketConnected, setSocketConnected] = useState(false);
+  const [myconfirm, setMyconfirm] = useState(""); // refresh 함수로 사용됩니다
+  const [socketConnected, setSocketConnected] = useState(true);
   const [auth_status, setAuth_status] = useState("");
   // const [loginjson, setLoginjson] = useState("");
   // let json_date = "791031-21:00:00";
@@ -16,31 +16,56 @@ export default function Home() {
   // const websocketUrl = "ws://utylee.duckdns.org/youtube/api/ws";
   const websocketUrl = "ws://utylee.duckdns.org/youtube/uploader/ws";
   let ws = useRef(null);
-  // let wsMsg = useRef("");
-  // let auth_status = useRef("idle");
-  // let auth_status = "idle";
+  // let ws_connected = useRef(false);
 
+  /*
   useEffect(() => {
     if (socketConnected) {
       console.log("sending msg..");
       ws.current.send("connected responsing send");
     }
   }, [socketConnected]);
+  */
 
   // useEffect(() => {}, [socketMessage]);
 
+  // 최초실행: 소켓 연결 함수 연결을 위해 set해줍니다
   useEffect(() => {
-    if (!ws.current) {
+    setSocketConnected(false);
+  }, []);
+
+  // 소켓 연결 useEffect
+  useEffect(() => {
+    console.log("index.js::useEffect[socketConnected]::ws.current is... ");
+    console.log(ws.current);
+    if (!ws.current && socketConnected == false) {
+      // if (!ws.current || ws_connected.current == false) {
       ws.current = new WebSocket(websocketUrl);
 
       ws.current.onopen = () => {
-        console.log("ws connected");
+        console.log("index.js::ws connected");
         setSocketConnected(true);
+        // ws_connected.current = true;
+
+        // 굳이 필요없는 것 같습니다
+        //setSocketConnected(true);
+
         // setMyconfirm(Math.random());
       };
 
+      ws.current.onclose = (evt) => {
+        // ws_connected.current = false;
+        console.log(
+          "index.js::useEffect[socketConnected]::websocket closed ",
+          evt
+        );
+        ws.current.close();
+        console.log("index.js::useEffect[socketConnected]::socket closing");
+        // ws.current = null;
+      };
+
       ws.current.onmessage = (evt) => {
-        console.log("comming msg:" + evt.data);
+        console.log("index.js::useEffect[socketConnected]::msg:" + evt.data);
 
         // parsed = JSON.parse(evt.data);
         // if (typeof parsed.type != "undefined && parsed.type) {
@@ -68,18 +93,21 @@ export default function Home() {
         }
       };
     }
-  }, []);
+  }, [socketConnected]);
+  //}, []);
 
   useEffect(() => {
-    console.log("index.js::useEffect");
+    console.log("index.js::useEffect[myconfirm] for /api/listjs");
     const getItems = async () => {
       console.log("came index.js::useEffect");
       const res = await fetch("/youtube/api/listjs");
       // const res = await fetch("/uploader/api/listjs");
       let js = await res.json();
       js = JSON.parse(js);
-      console.log("index.js::useEffect::js::" + js);
-      console.log("js.json_date:" + js.json_date);
+      console.log("index.js::useEffect::js::");
+      console.log(js);
+      console.log("js.json_date:");
+      console.log(js.json_date);
       setFiles({ ...js });
       // setFiles([...js]);
       // console.log("dddd");
@@ -95,7 +123,13 @@ export default function Home() {
         {/* <LoginJson auth_status={auth_status.current} /> */}
         {/* <LoginJson auth_status={auth_status} /> */}
         {/* <LoginJson json_date={json_date} auth_status={auth_status} /> */}
-        <LoginJson json_date={files.json_date} auth_status={auth_status} />
+        <LoginJson
+          json_date={files.json_date}
+          auth_status={auth_status}
+          setSocketConnected={setSocketConnected}
+          refresh_function={setMyconfirm}
+          ws={ws}
+        />
 
         {/* <Flex p={3} bg="gray.500" rounded="md"> */}
         {/*   핑두 */}
